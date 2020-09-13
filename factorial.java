16cc920aa1313da04261bc0f4c56104ec77b7769
VelammalPra <71201833+VelammalPra@users.noreply.github.com>
2020-09-13 09:52:53 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class factorial {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int fact=1;
+		int i=5;
+		for(i=1;i<=5;i++) {
+			fact=fact*i;
+			
+		}
+System.out.println(fact);
+	}
+
+}
